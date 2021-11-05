114d84ccc0a5574803039ca3c90d3fb79ffca865
haresh musti <hareshkm999@gmail.com>
2021-11-05 09:42:42 +0530

    Base file ready for callbacks

---
+++ src/utils/callbacks.py
@@ -0,0 +1,9 @@
+
+
+
+def create_and_save_tensorboard_callbacks(callbacks_dir, tensorboard_log_dir):
+    pass
+
+
+def create_and_save_checkpoint_callbacks(callbacks_dir, tensorboard_log_dir):
+    pass
