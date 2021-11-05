e449a4e71d9e369f8f71274da891b396662aeca7
haresh musti <hareshkm999@gmail.com>
2021-11-05 14:20:49 +0530

    stage 04 updated

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,4 +1,6 @@
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import load_full_model
+from src.utils.callbacks import get_callbacks
 from src.utils.callbacks import create_and_save_tensorboard_callbacks, create_and_save_checkpoint_callbacks
 import argparse
 import os
@@ -17,7 +19,18 @@ def train_model(config_path, params_path):
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
-    
+    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"] )
+
+    create_directory([train_model_dir_path])
+
+    untrained_full_model_path = os.path.join(artifacts_dir, artifacts["BASE_MODEL_DIR"], artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    model = load_full_model(untrained_full_model_path)
+
+    callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+    callbacks = get_callbacks(callback_dir_path)
+
+
 
 
 if __name__ == '__main__':
