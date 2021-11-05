38c835329554fde68b00d6e2d816b471b238eadb
haresh musti <hareshkm999@gmail.com>
2021-11-05 07:26:32 +0530

    stage 02 compleated

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,19 +1,15 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
-import pandas as pd
 import os
-import shutil
-from tqdm import tqdm
 import logging
+import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
-
-
 
 def prepare_base_model(config_path, params_path):
     config = read_yaml(config_path)
@@ -27,20 +23,43 @@ def prepare_base_model(config_path, params_path):
 
     base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
 
-    create_directory(base_model_dir_path)
+    create_directory([base_model_dir_path])
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
     model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path=base_model_path)
-    
 
-   
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=True,
+        freeze_till=None,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts["UPDATED_BASE_MODEL_NAME"]
+    )
+
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    full_model.save(update_base_model_path)
+    
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
+
     args.add_argument("--config", "-c", default="config/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
+
     parsed_args = args.parse_args()
 
     try:
@@ -50,3 +69,4 @@ if __name__ == '__main__':
     except Exception as e:
         logging.exception(e)
         raise e
+        
